fb19ce70b72aec41029d5cd57a0d997239d32d18
shisonoha <outboundport25blocking@gmail.com>
2025-10-18 09:08:46 +0900

    add C2510163.py

---
+++ ADT/C/C2510163.py
@@ -0,0 +1,15 @@
+# ADT2510163
+# https://atcoder.jp/contests/adt_medium_20251016_3/tasks/abc352_b
+
+S_STR = input()
+T_STR = input()
+
+s_index = 0
+answer = []
+for t_index in range(len(T_STR)):
+  if S_STR[s_index] == T_STR[t_index]:
+    answer.append(t_index + 1)
+    s_index += 1
+  t_index += 1
+
+print(*answer)
